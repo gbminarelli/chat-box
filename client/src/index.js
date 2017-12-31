39380e8bcc255e6d72a5bfe0e0304920591335eb
Gianlucci Badiali Minarelli <gianlucci.minarelli@gmail.com>
2017-12-30 18:50:57 -0800

    Bundle modules into index.js and set up the page layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,58 @@
 import io from 'socket.io-client';
-import RootComponent from './client.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Form, FormGroup, FormControl, Col, Button, InputGroup, Jumbotron } from 'react-bootstrap';
 
 const socket = io('http://localhost:8080'); //TODO change to the external ip address
 
+class Root extends React.Component {
+  render() {
+    return(
+      <Form horizontal>
+        <Display />
+        <Input />
+      </Form>
+    );
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    return(
+      <FormGroup>
+        <Col xs = {12}>
+          <Jumbotron id = 'display'>
+          </Jumbotron>
+        </Col>
+      </FormGroup>
+    );
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return(
+      <FormGroup controlId="formTextInput">
+        <Col xs = {12}>
+          <InputGroup>
+              <FormControl
+                type = 'text'
+                placeholder = 'Enter message...'
+              />
+              <InputGroup.Button>
+                <Button>
+                  Send
+                </Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </Col>
+      </FormGroup>
+    );
+  }
+}
+
 ReactDOM.render(
-  <RootComponent />, document.getElementById('root')
+  <Root />, document.getElementById('root')
 );
